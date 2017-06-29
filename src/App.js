07fb24a49bda18ede0a4ed26a01af903a2d519f7
Cameron Bell <cameronbell@gmail.com>
2017-06-28 19:32:05 -0600

    Basic Connection to GraphQL Server
    Implemented basic data fetching via GraphQL query in the app

--- src/App.js
+++ src/App.js
@@ -1,18 +1,32 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { QueryRenderer, graphql } from "react-relay";
+import environment from "./createRelayEnvironment";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="">
+        <h2>Relay Test</h2>
+        <QueryRenderer
+          environment={environment}
+          query={graphql`
+            query AppQuery {
+              node(id:"posts:1") {
+                ... on Post {
+                  body
+                }
+              }
+            }`}
+          render={({ error, props }) => {
+            if (error) {
+              return <div>{error.message}</div>;
+            } else if (props) {
+              console.log(props.node.body);
+              return <div>{props.node.body}</div>
+            }
+            return <div>Loading</div>;
+          }}
+        />
       </div>
     );
   }
